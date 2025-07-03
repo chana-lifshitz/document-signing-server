b44f76f7dc248bc5e1650b3b09aac5bdeaf1d399
chana-lifshitz <chana52458@gmail.com>
2025-07-04 01:10:31 +0300

    תקן Binding ל־0.0.0.0 עבור Render

--- server/server.js
+++ server/server.js
@@ -6,22 +6,22 @@ const cors = require('cors');
 const path = require('path');
 const documentRoutes = require('./Routs/documentRoutes');
 
-const host_name = process.env.HOST_NAME || '127.0.0.1';
-const port = process.env.PORT;
+// const host_name = process.env.HOST_NAME || '127.0.0.1';
+// const port = process.env.PORT;
+const port = process.env.PORT || 10000;
+
 app.use(cors()); // חשוב אם הלקוח בריאקט על פורט אחר
 app.use(express.json({ limit: "10mb" })); // נדרש בגלל base64
 // app.use(express.json()); // לפרוס JSON בבקשות POST
 // כדי לשרת את התמונות מתיקיית uploads בכתובת /image/
-app.use('/image', express.static(path.join(__dirname, 'uploads')));
+// app.use('/image', express.static(path.join(__dirname, 'uploads')));
 // ייבוא הנתיבים
 app.use(express.json());
 
 app.use('/api/documents', documentRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
 // app.use('/image', express.static(path.join(__dirname,'image')));
-
 
 app.use((err, req, res, next) => {
   if (err instanceof URIError) {
@@ -31,9 +31,12 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
+// app.listen(port, host_name, () => {
+//         console.log(`server is up in address http://${host_name}:${port}`);
 
-app.listen(port, host_name, () => {
-        console.log(`server is up in address http://${host_name}:${port}`);
-
+// });
+// שינוי כאן — מאזין על 0.0.0.0 ולא על localhost
+app.listen(port, '0.0.0.0', () => {
+  console.log(`server is up on port ${port}`);
 });
 // app.listen(3000, () => console.log("Server running on port 3000"));
